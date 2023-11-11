0285dd9ce9536c268664ced0c4159a3c23c7f954
Piyush Kumar <piyu8h@gmail.com>
2023-11-11 07:17:48 +0530

    Loadin... position Fixed

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className='flex h-screen justify-center bg-primary-col align-middle'>
-      <div className='relative mt-[calc(100vh/2)] h-min animate-pulse px-5 text-3xl font-extrabold text-white md:px-8 lg:text-5xl'>
+    <div className='flex h-screen items-center justify-center bg-primary-col'>
+      <div className='h-min animate-pulse px-5 text-3xl font-extrabold text-white md:px-8 lg:text-5xl'>
         Loading&nbsp;...
       </div>
     </div>
